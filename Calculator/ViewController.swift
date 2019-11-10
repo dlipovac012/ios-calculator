6bfc1963b7b3cdd06843830bb4b5ae8772c15b46
Dalibor Lipovac <dlipovac012@gmail.com>
2019-11-10 20:31:33 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dalibor Lipovac on 10/11/2019.
+//  Copyright © 2019 Dalibor Lipovac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
